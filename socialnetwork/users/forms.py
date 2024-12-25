6a54926bffa80cb25301421ed00e7cf879fb6052
sinatb <seyedalitabatabaeishere@gmail.com>
2024-12-25 17:06:37 +0330

    login and signup form

---
+++ socialnetwork/users/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+
+class SignupForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+        labels = {
+            "email":"ایمیل",
+        }
+    username = forms.CharField(label=("نام کاربری"))
+    password1 = forms.CharField(label=("گذرواژه"),widget=forms.PasswordInput(
+        attrs={'placeholder':'گذرواژه را وارد کنید'}
+    ))
+    password2 = forms.CharField(label=(" تکرار گذرواژه "),widget=forms.PasswordInput(
+        attrs={'placeholder':'تکرار گذرواژه را وارد کن'}
+    ))
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(label=("نام کاربری"))
+    password = forms.CharField(label=("گذرواژه"),widget=forms.PasswordInput(attrs={'placeholder':'گذرواژه را وارد کنید'}))
--- socialnetwork/users/views.py
+++ socialnetwork/users/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.contrib.auth import login, authenticate
+from .forms import SignupForm,LoginForm
 
-# Create your views here.
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            return redirect('login/')
+    else:
+        form = SignupForm()
+    return render(request, 'signup.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            response = f"{user} عزیز شما با موفقیت وارد شدید"
+            print(response)
+            return redirect('../profile/')
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
+
+
+@login_required
+def profile_view(request):
+    user = request.user
+    return render(request, 'profile.html', {'user': user})
