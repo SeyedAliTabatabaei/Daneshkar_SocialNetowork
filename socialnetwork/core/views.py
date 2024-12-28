d3409661d3cf7df30e02af00758f3b7f0c9ab0be
sinatb <seyedalitabatabaeishere@gmail.com>
2024-12-28 19:52:58 +0330

    like / comment button added !

--- socialnetwork/core/views.py
+++ socialnetwork/core/views.py
@@ -1,9 +1,33 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from users.models import Post,Profile
+from django.utils.timesince import timesince
+from django.utils.timezone import now
 
+def translate_timesince(time_diff):
+    english_time = timesince(time_diff,now())
+
+    translated_time = (
+        english_time.replace("minutes", "دقیقه")
+        .replace("minute", "دقیقه")
+        .replace("hours", "ساعت")
+        .replace("hour", "ساعت")
+        .replace("days", "روز")
+        .replace("day", "روز")
+        .replace("weeks", "هفته")
+        .replace("week", "هفته")
+    )
+    return f"{translated_time} پیش"
 
 def home(request):
-    posts = Post.objects.all().order_by('-created_at')
+    allposts = Post.objects.all().order_by('-created_at')
+    current_time = now()
+
+    posts = []
+    for post in allposts:
+        posts.append({
+            'post': post,
+            'time_ago': f"{translate_timesince(post.created_at)}"
+        })  
     return render(request, 'home.html', {'posts':posts})
 
--- socialnetwork/users/models.py
+++ socialnetwork/users/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    profile_image = models.ImageField(upload_to='profile_images/', default="profile_images/default.png", blank=True, null=True)
+    profile_image = models.ImageField(upload_to='profile_images/', default="profile_images/default.png", blank=False, null=False)
     bio = models.TextField(max_length=100,blank=True, null=True)
 
 class Post(models.Model):
